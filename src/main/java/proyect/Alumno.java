9f44042919f2eed029e51f1bc0066be50d5ff5a6
Arturo Rivas <a.rivas06@ufromail.cl>
2024-04-06 01:31:06 -0300

    Crea constructor Alumno

--- src/main/java/proyect/Alumno.java
+++ src/main/java/proyect/Alumno.java
@@ -2,7 +2,12 @@ package proyect;
 import java.util.ArrayList;
 
 public class Alumno {
-    String matricula;
     String nombre;
+    String matricula;
     ArrayList<Asignatura> asignaturasAprobadas = new ArrayList<>();
+
+    Alumno(String nombre, String matricula){
+        this.nombre = nombre;
+        this.matricula = matricula;
+    }
 }
