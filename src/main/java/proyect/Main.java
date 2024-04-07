855c76cc89f0516e9bd897e7a73569836277a314
Cris <c.cheuquel01@ufromail.cl>
2024-04-06 23:38:56 -0300

    Ruta del archivo asignaturasInformatica.csv cambiada

--- src/main/java/proyect/Main.java
+++ src/main/java/proyect/Main.java
@@ -4,7 +4,7 @@ package proyect;
 public class Main {
     public static void main(String[] args) {
         Asignaturas asignaturasInformatica = new Asignaturas();
-        asignaturasInformatica.leerAsignaturas("resource/asignaturasInformatica.csv");
+        asignaturasInformatica.leerAsignaturas("asignaturasInformatica.csv");
         asignaturasInformatica.imprimirAsignaturas();
     }
 
