63f7546c60a5765c3be2ca19a9273c6ddb11d1c1
Cris <c.cheuquel01@ufromail.cl>
2024-04-06 23:35:02 -0300

    ñe

--- src/main/java/proyect/Main.java
+++ src/main/java/proyect/Main.java
@@ -4,8 +4,8 @@ package proyect;
 public class Main {
     public static void main(String[] args) {
         Asignaturas asignaturasInformatica = new Asignaturas();
-        asignaturasInformatica.leerAsignaturas("C:/Users/alvar/OneDrive/Escritorio/Proyecto_POO/asignaturasInformatica.csv");
-        asignaturasInformatica.imprimirAsignaturas();      
+        asignaturasInformatica.leerAsignaturas("resource/asignaturasInformatica.csv");
+        asignaturasInformatica.imprimirAsignaturas();
     }
 
 }
