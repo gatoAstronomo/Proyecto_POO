b6da0d5e6ece59a2c26572a67d4849ab8e5857db
Cris <c.cheuquel01@ufromail.cl>
2024-04-07 00:29:41 -0400

    ahora se usan los getters y setters de la clase Asignatura

--- src/main/java/proyect/DataBase.java
+++ src/main/java/proyect/DataBase.java
@@ -9,38 +9,37 @@ public class DataBase{
     ArrayList<Asignatura> asignaturas = new ArrayList<Asignatura>();
 
     public void leerAsignaturas(String archivo) {
-        ArrayList<Asignatura> asignaturas = new ArrayList<Asignatura>();
+        //ArrayList<Asignatura> asignaturas = new ArrayList<Asignatura>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(archivo));
             String linea;
             while ((linea = br.readLine()) != null) {
                 String[] datos = linea.split(",");
-                String nombre = datos[0];
-                int numeroId = Integer.parseInt(datos[1]);
-                int nivel = Integer.parseInt(datos[2]);
-                int horasSct = Integer.parseInt(datos[3]);
-                ArrayList<Integer> idPrerrequisitos = new ArrayList<Integer>();
+
+                Asignatura ramo = new Asignatura();
+                ramo.setNombre(datos[0]);
+                ramo.setNumeroId(Integer.parseInt(datos[1]));
+                ramo.setNivel(Integer.parseInt(datos[2]));
+                ramo.setHorasSct(Integer.parseInt(datos[3]));
+
                 for(int i = 4; i < datos.length; i++){
                     int requisito = Integer.parseInt(datos[i]);
-                    idPrerrequisitos.add(requisito);
+                    ramo.addIdPrerrequisito(requisito);
                 }
-                    
-                Asignatura asignatura = new Asignatura(nombre, numeroId, nivel, horasSct, idPrerrequisitos);
-                asignaturas.add(asignatura);
+                this.asignaturas.add(ramo);
             }
             br.close();
         } catch (IOException e) {
             System.out.println("Error al leer el archivo");
         }
-        this.asignaturas = asignaturas;
     }
 
     public void imprimirAsignaturas(){
         for (Asignatura asignatura : this.asignaturas) {
-            System.out.println("Nombre: " + asignatura.nombre);
-            System.out.println("Número ID: " + asignatura.numeroId);
-            System.out.println("Nivel: " + asignatura.nivel);
-            System.out.println("Horas SCT: " + asignatura.horasSct);
+            System.out.println("Nombre: " + asignatura.getNombre());
+            System.out.println("Número ID: " + asignatura.getNumeroId());
+            System.out.println("Nivel: " + asignatura.getNivel());
+            System.out.println("Horas SCT: " + asignatura.getHorasSCT());
             System.out.print("Prerrequisitos: " + asignatura.idPrerrequisitos.get(0));
             for(int i = 1; i < asignatura.idPrerrequisitos.size(); i++) {
                 System.out.print(", " + asignatura.idPrerrequisitos.get(i));
