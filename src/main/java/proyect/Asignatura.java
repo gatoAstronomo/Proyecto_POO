bf44d41d3e925085b81bb4006cf6f24336f2d840
Cris <c.cheuquel01@ufromail.cl>
2024-04-07 12:40:51 -0400

    Asignatura.imprimirAsignatura(): se agregó un \n

--- src/main/java/proyect/Asignatura.java
+++ src/main/java/proyect/Asignatura.java
@@ -25,7 +25,7 @@ public class Asignatura{
         System.out.println("Nivel: " + this.nivel);
         System.out.println("Horas SCT: " + this.horasSct);
         imprimirPrerrequisitos();
-        System.err.println("\n");
+        System.out.println("\n");
     }
     public void imprimirPrerrequisitos(){
         System.out.print("Prerrequisitos: " + this.idPrerrequisitos.get(0));
