0a783f7023ad81a7de1cd530aff6ac8798f4c880
HectorBecerra <h.becerra01@ufromail.cl>
2024-04-07 16:36:31 -0400

    mejora de menu y control de excepciones

--- src/main/java/proyect/Main.java
+++ src/main/java/proyect/Main.java
@@ -3,6 +3,6 @@ package proyect;
 
 public class Main {
     public static void main(String[] args){
-        Menu.main(args);
+        Menu.prenderMenu(args);
     }
 }
--- src/main/java/proyect/Menu.java
+++ src/main/java/proyect/Menu.java
@@ -1,48 +1,70 @@
 package proyect;
 
-
 import java.util.Scanner;
 
 public class Menu {
+
+    public static void imprimirMenu(){
+        System.out.println("***** Menú *****");
+        System.out.println("1. Ver lista de asignaturas");
+        System.out.println("2. Buscar asignatura por ID");
+        System.out.println("3. Salir");
+    }
+    
+    public static int obtenerOpcion(Scanner scanner){
+        try {
+            System.out.print("Ingrese una opción: ");
+            return scanner.nextInt();
+        } catch (Exception e) {
+            scanner.nextLine();
+            return -1;
+        }    
+    }
+
+    public static void procesarOpcion(int opcion, DataBase asignaturasInformatica, Scanner scanner) {
+        switch (opcion) {
+            case 1:
+                System.out.println("Asignaturas de la carrera de Ingenieria Civil Informatica");
+                asignaturasInformatica.imprimirAsignaturas();
+                break;
+            case 2:
+                buscarAsignaturaPorId(asignaturasInformatica, scanner);
+                break;
+            case 3:
+                System.out.println("Saliendo del programa...");
+                break;
+            default:
+                System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+                break;        
+        }
+    }
+
+    public static void buscarAsignaturaPorId(DataBase asignaturasInformatica, Scanner scanner) {
+        System.out.print("Ingrese el número de ID de la asignatura a buscar: ");
+        int id = scanner.nextInt();
+        Asignatura asignaturaEncontrada = asignaturasInformatica.buscarAsignaturaPorId(id);
+        if (asignaturaEncontrada != null) {
+            System.out.println("Asignatura encontrada:");
+            asignaturaEncontrada.imprimirAsignatura();
+        } else {
+            System.out.println("No se encontró ninguna asignatura con ese ID.");
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean salir = false;
         DataBase asignaturasInformatica = new DataBase();
         asignaturasInformatica.leerAsignaturas("asignaturasInformatica.csv");
-
         while (!salir) {
-            System.out.println("***** Menú *****");
-            System.out.println("1. Ver lista de asignaturas");
-            System.out.println("2. Buscar asignatura por ID");
-            System.out.println("3. Salir");
-            System.out.print("Ingrese el número de la opción deseada: ");
-            int opcion = scanner.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    System.out.println("Lista de asignaturas:");
-                    asignaturasInformatica.imprimirAsignaturas();
-                    break;
-                case 2:
-                    System.out.print("Ingrese el número de ID de la asignatura a buscar: ");
-                    int id = scanner.nextInt();
-                    Asignatura asignaturaEncontrada = asignaturasInformatica.buscarAsignaturaPorId(id);
-                    if (asignaturaEncontrada != null) {
-                        System.out.println("Asignatura encontrada:");
-                        asignaturaEncontrada.imprimirAsignatura();
-                    } else {
-                        System.out.println("No se encontró ninguna asignatura con ese ID.");
-                    }
-                    break;
-                case 3:
-                    System.out.println("Saliendo del programa...");
-                    salir = true;
-                    break;
-                default:
-                    System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+            imprimirMenu();
+            int opcion = obtenerOpcion(scanner);
+            procesarOpcion(opcion, asignaturasInformatica, scanner);
+            if (opcion == 3) {
+                salir = true;
             }
-        }
-        scanner.close();
+        } 
+        scanner.close();  
     }
 }
+
