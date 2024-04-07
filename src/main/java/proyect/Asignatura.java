7c06241dc519c95a2b427b556d2f97b2003a3404
Cris <c.cheuquel01@ufromail.cl>
2024-04-06 23:12:38 -0400

    Getters y setters creados para la clase Asignatura

--- src/main/java/proyect/Asignatura.java
+++ src/main/java/proyect/Asignatura.java
@@ -16,5 +16,31 @@ public class Asignatura{
         this.horasSct = horasSct;
         this.idPrerrequisitos = idPrerrequisitos;
     }
+    // Getters y Setters
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getNumeroId(){
+        return this.numeroId;
+    }
+    public void setNumeroId(int numeroId){
+        this.numeroId = numeroId;
+    }
+    public int getNivel(){
+        return this.nivel;
+    }
+    public void setNivel(int nivel){
+        this.nivel = nivel;
+    }
+    public int getHorasSCT(){
+        return this.horasSct;
+    }
+    public void setHorasSct(int horasSct){
+        this.horasSct = horasSct;
+    }
 
 }   
