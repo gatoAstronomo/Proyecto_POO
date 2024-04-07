98b1807f73372415288b9b28cdf598968ec6def4
HectorBecerra <h.becerra01@ufromail.cl>
2024-04-07 00:28:18 -0400

    cambios en DataBase

--- src/main/java/proyect/DataBase.java
+++ src/main/java/proyect/DataBase.java
@@ -14,25 +14,33 @@ public class DataBase{
             BufferedReader br = new BufferedReader(new FileReader(archivo));
             String linea;
             while ((linea = br.readLine()) != null) {
-                String[] datos = linea.split(",");
-                String nombre = datos[0];
-                int numeroId = Integer.parseInt(datos[1]);
-                int nivel = Integer.parseInt(datos[2]);
-                int horasSct = Integer.parseInt(datos[3]);
-                ArrayList<Integer> idPrerrequisitos = new ArrayList<Integer>();
-                for(int i = 4; i < datos.length; i++){
-                    int requisito = Integer.parseInt(datos[i]);
-                    idPrerrequisitos.add(requisito);
-                }
-                    
-                Asignatura asignatura = new Asignatura(nombre, numeroId, nivel, horasSct, idPrerrequisitos);
-                asignaturas.add(asignatura);
+                asignaturas.add(procesarLinea(linea));
             }
             br.close();
         } catch (IOException e) {
             System.out.println("Error al leer el archivo");
         }
         this.asignaturas = asignaturas;
+    }
+
+    private Asignatura procesarLinea(String linea) {
+        String[] datos = linea.split(",");
+        String nombre = datos[0];
+        int numeroId = Integer.parseInt(datos[1]);
+        int nivel = Integer.parseInt(datos[2]);
+        int horasSct = Integer.parseInt(datos[3]);
+        ArrayList<Integer> idPrerrequisitos = obtenerPrerrequisitos(datos);
+        Asignatura asignatura = new Asignatura(nombre, numeroId, nivel, horasSct, idPrerrequisitos);
+        return asignatura;
+    }
+
+    private ArrayList<Integer> obtenerPrerrequisitos(String[] datos) {
+        ArrayList<Integer> idPrerrequisitos = new ArrayList<Integer>();
+        for (int i = 4; i < datos.length; i++) {
+            int requisito = Integer.parseInt(datos[i]);
+            idPrerrequisitos.add(requisito);
+        }
+        return idPrerrequisitos;
     }
 
     public void imprimirAsignaturas(){
@@ -42,11 +50,15 @@ public class DataBase{
             System.out.println("Nivel: " + asignatura.nivel);
             System.out.println("Horas SCT: " + asignatura.horasSct);
             System.out.print("Prerrequisitos: " + asignatura.idPrerrequisitos.get(0));
-            for(int i = 1; i < asignatura.idPrerrequisitos.size(); i++) {
+            imprimirPrerrequisitos(asignatura);
+        }
+    }
+
+    public void imprimirPrerrequisitos(Asignatura asignatura){
+        for(int i = 1; i < asignatura.idPrerrequisitos.size(); i++) {
                 System.out.print(", " + asignatura.idPrerrequisitos.get(i));
             }
             System.out.println("\n");
-        }
     }
 
     public ArrayList<Asignatura> getAsignaturas(){
