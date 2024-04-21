0afdd88265aa5d604210d862e5e18c1f301e0a21
Arturo Rivas <a.rivas06@ufromail.cl>
2024-04-21 18:05:21 -0300

    Copia métodos de Alumno de la rama cris

--- src/main/java/proyect/Alumno.java
+++ src/main/java/proyect/Alumno.java
@@ -6,17 +6,46 @@ public class Alumno {
     String nombre;
     String matricula;
     ArrayList<Integer> idAsignaturasAprobadas = new ArrayList<Integer>();
+    ArrayList<String> nombrePrerrequisitos = new ArrayList<String>();
 
-    Alumno(String nombre, String matricula){
+    public Alumno(String nombre, String matricula){
         this.nombre = nombre;
         this.matricula = matricula;
     }
+
+    public Alumno(){}
 
     Alumno(String nombre, String matricula, ArrayList<Integer> idAsignaturasAprobadas) {
         this.nombre = nombre;
         this.matricula = matricula;
         this.idAsignaturasAprobadas = idAsignaturasAprobadas;
     }
+
+    public void agregarAsignaturaAprobada(int id){
+        this.idAsignaturasAprobadas.add(id);
+    }
+
+    public void addIdAsignaturaAprobada(int idAsignaturaAprobada){
+        this.idAsignaturasAprobadas.add(idAsignaturaAprobada);
+    }
+    // Setters y Getters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+    public String getMatricula(){
+        return this.matricula;
+    }
+    public void setIdAsignaturasAprobadas(ArrayList<Integer> idAsignaturasAprobadas){
+        this.idAsignaturasAprobadas = idAsignaturasAprobadas;
+    }
+    public ArrayList<Integer> getIdAsignaturasAprobadas(){
+        return this.idAsignaturasAprobadas;
+    }
+
 }
-
-
