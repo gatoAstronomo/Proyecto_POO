3f0e1593d678fd8754d002a3490c6d4c4a0402d9
Arturo Rivas <a.rivas06@ufromail.cl>
2024-05-18 04:43:28 -0300

    Arregla los test unitarios

--- src/test/java/database/DataBaseTest.java
+++ src/test/java/database/DataBaseTest.java
@@ -19,7 +19,7 @@ class DataBaseTest {
 
     @Test
     void buscarAsignaturaPorId() {
-        DataBase db = new DataBase("/src/main/java/resources/asignaturas.csv","/src/main/java/resources/alumnos.csv");
+        DataBase db = new DataBase("src/main/java/resources/asignaturas.csv","src/main/java/resources/alumnos.csv");
         Asignatura asignatura = db.buscarAsignaturaPorId(15);
         assertEquals(asignatura.getNombre(), "Calculo Multivariable");
 
@@ -27,25 +27,25 @@ class DataBaseTest {
 
     @Test
     void buscarCoincidenciasPorNombre() {
-        DataBase db = new DataBase("/src/main/java/resources/asignaturas.csv","/src/main/java/resources/alumnos.csv");
+        DataBase db = new DataBase("src/main/java/resources/asignaturas.csv","src/main/java/resources/alumnos.csv");
         assertEquals(db.buscarCoincidenciasPorNombre("Calculo Multivariable").size(), 1);
     }
 
     @Test
     void buscarCoincidenciasPorNombre2() {
-        DataBase db = new DataBase("/src/main/java/resources/asignaturas.csv","/src/main/java/resources/alumnos.csv");
+        DataBase db = new DataBase("src/main/java/resources/asignaturas.csv","src/main/java/resources/alumnos.csv");
         assertNotEquals(db.buscarCoincidenciasPorNombre("Calculo").size(), 2);
     }
 
     @Test
     void noesAlumno() {
-        DataBase db = new DataBase("/src/main/java/resources/asignaturas.csv","/src/main/java/resources/alumnos.csv");
+        DataBase db = new DataBase("src/main/java/resources/asignaturas.csv","src/main/java/resources/alumnos.csv");
         assertFalse(db.esAlumno("201800000"));
     }
 
     @Test
     void esAlumno() {
-        DataBase db = new DataBase("/src/main/java/resources/asignaturas.csv","/src/main/java/resources/alumnos.csv");
+        DataBase db = new DataBase("src/main/java/resources/asignaturas.csv","src/main/java/resources/alumnos.csv");
         assertTrue(db.esAlumno("22123123720"));
     }
 
